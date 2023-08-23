708f92837e36f6b7709c58787acdc6cad973c13c
SousMour <142849312+SousMour@users.noreply.github.com>
2023-08-23 19:31:23 -0300

    Add files via upload

---
+++ 01 Arte em ASCII.py
@@ -0,0 +1,22 @@
+print("Oi, tudo bem?")
+print("Estou com fome")
+print("Uma imagem de cachorro...")
+print(" 0_____ ")
+print("  ||||  ")
+print(''' Uma imagem de cachorro...
+0_____
+||||
+''')
+
+print(" Olá" * 5)
+
+print("Olá," + "tudo bem?")
+
+print("ha" * 4)
+print("ba" + "na"*2)
+print("Bra" + "sil" + "!"*10)
+print("#"*30)
+print("#"*30)
+print("#"*30)
+
+print("/\ " * 30) 
